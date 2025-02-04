cbba6eadb13e14dc2f84cbc3e21349d69ee54d46
Javier Eduardo Espinoaz Leal <a22311074@uthermosillo.edu.mx>
2025-02-04 04:03:18 -0700

    Crear componente lista de tareas. Borra componenete autogenerador. Crea rutas

---
+++ src/components/ListaDeTareas.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2> Lista de Tareas</h2>
+        <input type="text" v-model="nuevatarea" @keyup.enter="addtarea">
+        <ul>
+            <li v-for="(tarea, index) in tareas" :key="index">
+                {{ tarea }}
+                <button @click="borrarTarea">borrar</button>
+            </li>
+        </ul>
+    </div>
+</template>
+<script setup>
+import {ref} from 'vue';
+const tareas = ref([]);
+const nuevatarea = ref ('');
+const addtarea = () => {
+    tareas.value.push(nuevatarea.value);
+    nuevatarea.value = '';
+}
+
+const borrarTarea = (index) => {
+    tareas.value.splice(index,1);
+}
+</script>
+<style></style>
